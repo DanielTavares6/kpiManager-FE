620db38348933b09ddff166e55969fbdf0b2589d
danielrossi6 <danieljosetavares@gmail.com>
2020-04-30 17:34:03 +0100

    bug fix

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -45,7 +45,7 @@ const routes: Routes = [
         redirectTo: "layoutform",
         pathMatch: "full"
       }, */
-      { path: "", redirectTo: "statistics", pathMatch: "full" },
+      { path: "", redirectTo: "dashboard", pathMatch: "full" },
     ],
   },
 ];
